7b6a646f0d9a08db46e86606222fa4d697848935
David <david.me.public@gmail.com>
2019-12-09 01:04:19 +0900

    Side bar drawn

--- Sidebar.py
+++ Sidebar.py
@@ -1,7 +1,7 @@
 from tkinter import *
+from app_constants import Constants
 
 class Sidebar:
     def __init__(self, root):
-        side_frame = Frame(root, height = 300, width = 100 , bg = "#554F4C", relief="raised")
-        print(root.winfo_reqheight)
+        side_frame = Frame(root, height = Constants.app_window_height, width = Constants.app_window_width / 4 , bg = "#554F4C", relief="raised")
         side_frame.pack(side="left")
